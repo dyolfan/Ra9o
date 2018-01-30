ff46e2ab6188b63c326a1d28016e1597a8ed5422
Kirils Putimcevs <sonistar@yandex.ru>
2018-01-30 18:03:32 +0500

    Comleted	Extra02: 	LetsRide

--- src/main/java/jtm/extra02/ArrayListMethods.java
+++ src/main/java/jtm/extra02/ArrayListMethods.java
@@ -7,7 +7,7 @@ public class ArrayListMethods {
 	List<Integer> myList = new ArrayList<Integer>();
 
 	public List<Integer> checkArray(int comparator, int... numbers) {
-		// TODO #1:Implement method that compares values of passed array with
+		// #1:Implement method that compares values of passed array with
 		// passed comparator.
 		// Return list with values that are smaller than comparator.
 		// Hint: Investigate how varargs are used.
@@ -20,7 +20,7 @@ public class ArrayListMethods {
 
 	public int sumResult() {
 		int sum = 0;
-		// TODO #2: Count element sum of the list
+		// #2: Count element sum of the list
 		for (int number : myList)
 			sum+= number;
 		return sum;
--- src/main/java/jtm/extra02/LetsRide.java
+++ src/main/java/jtm/extra02/LetsRide.java
@@ -12,28 +12,41 @@ public class LetsRide {
 	int seatsCount;// bus seats count
 
 	public LetsRide(int busStopCount, int passengersInStop, int seatsCount) {
-		// TODO #1: Set passed values to LetsRide object
+		// #1: Set passed values to LetsRide object
+		this.busStopCount = busStopCount;
+		this.seatsCount = seatsCount;
+		this.passengersAtStart = passengersInStop;
 	}
 
 	public int passengersAtRouteEnd() {
-		// TODO #2: Calculate how many passengers will be in bus at the end of
+		// #2: Calculate how many passengers will be in bus at the end of
 		// route. Overall passenger count
 		// is incremented by bus stop number. Example: In bus stop No.1
 		// passenger count will be increased by 1, in stop No.2 it
 		// will be increased by 2 and so on until bus reaches route end.
 		// Note: Overall passenger count can't exceed seat count
+		passengersCount = passengersAtStart;
+		for(int i = 1; i <= busStopCount; i++){
+			passengersCount+=i;
+			if(passengersCount > seatsCount) {
+				passengersCount = seatsCount;
+			}
+		}
 		return passengersCount;
 	}
 
 	public int freeSeats() {
 		int freeSeats = 0;
-		// TODO #3: Calculate how much seats are free in bus
+		// #3: Calculate how much seats are free in bus
+		freeSeats = seatsCount - passengersCount;
 		return freeSeats;
 	}
 
 	public boolean isFull() {
 		boolean status = false;
-		// TODO #4: Check if bus is full.
+		// #4: Check if bus is full.
+		if (passengersCount == seatsCount)
+			status = true;
 		return status;
 	}
 
