80dbf581a816c34634a72bb0ea5231eb431dc113
Kirils Putimcevs <sonistar@yandex.ru>
2018-01-30 20:37:40 +0500

    Completed activity04

--- src/main/java/jtm/activity04/Road.java
+++ src/main/java/jtm/activity04/Road.java
@@ -5,26 +5,61 @@ public class Road {
 	private String to; // End point
 	private int distance; // distance in km
 
-	/*- TODO #1
+	/*- #1
 	 * Select menu Source — Generate Constructor using Fields...
 	 * and create constructor which sets from, to and distance
 	 * values of the newly created object
 	 */
+	
+	public Road(String from, String to, int distance) {
+		super();
+		this.from = from;
+		this.to = to;
+		this.distance = distance;
+	}
 
-	/*- TODO #2
+	/*-	#2
 	 * Create constructor without parameters, which sets empty
 	 * values or 0 to all object properties
 	 */
+	public Road() {
+		this.from = "";
+		this.to = "";
+		this.distance = 0;
+	}
 
-
-	/*- TODO #3
+	/*- #3
 	 * Select menu: Source — Generate getters and Setters...
 	 * and generate public getters/setters for distance, from and to
 	 * fields
 	 */
 
+	public String getFrom() {
+		return from;
+	}
 
-	/*- TODO #4
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+	
+
+	/*- #4
 	 * Select menu: Source — Generate toString()...
 	 * and implement this method, that it returns String in form:
 	 * "From — To, 00km"
@@ -32,5 +67,10 @@ public class Road {
 	 * See more at: https://en.wikipedia.org/wiki/Dash
 	 */
 
+	@Override
+	public String toString() {
+		return getFrom() + " — " + getTo()
+				+ ", " + getDistance() +"km";
+	}
 
 }
--- src/main/java/jtm/activity04/Transport.java
+++ src/main/java/jtm/activity04/Transport.java
@@ -9,48 +9,123 @@ public class Transport {
 	private int tankSize; // tank size in litres
 	private float fuelInTank; // fuel in tank
 
-	/*- TODO #1
+	/*- #1
 	 * Select menu Source — Generate Constructor using Fields...
 	 * and create constructor which sets id, consumption, tankSize
 	 * values of the newly created object
 	 * And make fuel tank full.
 	 */
+	
+	public Transport(String id, float consumption, int tankSize) {
+		super();
+		this.id = id;
+		this.consumption = consumption;
+		this.tankSize = tankSize;
+		this.fuelInTank = tankSize;
+	}
+	
 
 
-	/*- TODO #2
+	/*- #2
 	 * Select menu: Source — Generate getters and Setters...
 	 * and generate public getters for consumption, tankSize, id, and
 	 * fuelInTank fields
 	 */
+	
+	public String getId() {
+		return id;
+	}
 
-	/*- TODO #3
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+
+	public float getConsumption() {
+		return consumption;
+	}
+
+
+
+	public void setConsumption(float consumption) {
+		this.consumption = consumption;
+	}
+
+
+
+	public int getTankSize() {
+		return tankSize;
+	}
+
+
+
+	public void setTankSize(int tankSize) {
+		this.tankSize = tankSize;
+	}
+
+
+
+	public float getFuelInTank() {
+		return fuelInTank;
+	}
+
+
+
+	public void setFuelInTank(float fuelInTank) {
+		this.fuelInTank = fuelInTank;
+	}
+
+	/*- #3
 	 * Select menu: Source — Generate toString()...
 	 * and implement this method, that t returns String in form:
 	 * "Id:ID cons:0.0l/100km, tank:00l, fuel:00.00l"
 	 * 	HINT: use String.format(Locale.US, "%.2f", float) to limit shown numbers
 	 *  to 2 decimal for fractions, and dot as a decimal delimiter.
 	 */
-
+	
+	@Override
+	public String toString() {
+		return String.format("Id:%s cons:%.1fl/100km, tank:%dl, fuel:%.2fl", this.id, this.consumption, this.tankSize, this.fuelInTank);
+	}
 
 	// Return transport id and type as string e.g. "AAA Transport"
 	// HINT: use this.getClass().getSimpleName(); to get type of transport
 	protected final String getType() {
-		// TODO return required value
-		return "";
+		// return required value
+		return String.format("%s %s", this.getId(), this.getClass().getSimpleName());
 	}
+
 
 	// HINT: use getType() to describe transport and road.toString() to describe
 	// road
 	// HINT: String.format(Locale.US, "%.2f", float) to format float number with
 	// fixed mask
+	
 	public String move(Road road) {
-		// TODO If transport has enough fuel, decrease actual amount of fuel by
-		// necessary amount and return String in form:
-		// "AAA Type is moving on From–To, 180km"
-		// TODO If there is no enough fuel in tank, return string in form:
-		// "Cannot move on From–To, 180km. Necessary
-		// fuel:0.00l, fuel in tank:0.00l"
-		return "";
+	// If transport has enough fuel, decrease actual amount of fuel by
+	// necessary amount and return String in form:
+	// "AAA Type is moving on From–To, 180km"
+		
+		if(getNececeryFuel(road) <= this.getFuelInTank()){
+			fuelInTank -= this.getNececeryFuel(road);
+			return String.format("%s is moving on %s", this.getType(), road.toString());
+		}
+	
+	//  If there is no enough fuel in tank, return string in form:
+	// "Cannot move on From–To, 180km. Necessary
+	// fuel:0.00l, fuel in tank:0.00l"
+	
+		else return String.format("Cannot move on %s. Necessary fuel:%.2fl, fuel in tank:%.2fl", road.toString(), road.getDistance()*this.getConsumption()/100, this.getFuelInTank());
 	}
-
+	
+	public float getNececeryFuel(Road road) {
+		float consumptionPerKm = this.getConsumption()/100;
+		float necFuel = 0;
+		necFuel = road.getDistance()*consumptionPerKm;
+		return necFuel;
+	}
+	
 }
